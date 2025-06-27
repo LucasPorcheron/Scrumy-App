3e4afb8a3a60ae07bd1e8b1c766a550fdf567d76
LucasPorcheron <129221574+LucasPorcheron@users.noreply.github.com>
2025-06-27 11:48:41 +0200

    Update route.ts

--- app/api/story/[id]/route.ts
+++ app/api/story/[id]/route.ts
@@ -1,18 +1,4 @@
-import { prisma } from '@/lib/prisma'
-import { NextRequest } from 'next/server'
-
-// ✅ DELETE : supprimer une story
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  try {
-    await prisma.story.delete({
-      where: { id: params.id },
-    })
-    return new Response(null, { status: 204 })
-  } catch (err) {
-    console.error(err)
-    return new Response(JSON.stringify({ erreur: 'Erreur serveur' }), { status: 500 })
+export async function DELETE() {
+    return new Response('OK', { status: 200 })
   }
-}
+  
